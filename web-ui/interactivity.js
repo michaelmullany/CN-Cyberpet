7769edb861caf0af9e341d207decf5ea56d98a2c
Michael Mullany <mamullany@gmail.com>
2022-04-07 10:02:19 +0100

    Clicking an action button grabs the element id

--- web-ui/interactivity.js
+++ web-ui/interactivity.js
@@ -2,14 +2,18 @@ const selectPetScreen = document.getElementById("selectPet");
 const inGameScreen = document.getElementById("inGame");
 const selectDogButton = document.getElementById("selectDogButton");
 
+const petFeedback = document.getElementById("petFeedback");
+const actionButtons = document.getElementsByClassName("action");
+
 inGameScreen.style.display = "none";
 
 selectDogButton.addEventListener("click", () => {
-    console.log("Button Clicked");
     selectPetScreen.style.display = "none";
     inGameScreen.style.display = "block";
 });
 
-console.log(selectPetScreen);
-console.log(inGameScreen);
-console.log(selectDogButton);
+for (let i = 0; i < actionButtons.length; i++) {
+    actionButtons[i].addEventListener("click", () => {
+        petFeedback.textContent = actionButtons[i].id;
+    });
+};
