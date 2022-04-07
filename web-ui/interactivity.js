3af30ad7244ed20bf9b424277761c6288fb86a35
Michael Mullany <mamullany@gmail.com>
2022-04-07 13:50:54 +0100

    Hook up dog selection screen to JS

--- web-ui/interactivity.js
+++ web-ui/interactivity.js
@@ -2,15 +2,28 @@ const selectPetScreen = document.getElementById("selectPet");
 const inGameScreen = document.getElementById("inGame");
 const selectDogButton = document.getElementById("selectDogButton");
 
+const testText = document.getElementById("testText");
 const petFeedback = document.getElementById("petFeedback");
+
+const selectionButtons = document.getElementsByClassName("dogSelection");
 const actionButtons = document.getElementsByClassName("action");
 
 inGameScreen.style.display = "none";
+
+/* SELECT DOG SCREEN */
+
+for (let i = 0; i < selectionButtons.length; i++) {
+    selectionButtons[i].addEventListener("click", () => {
+        testText.textContent = selectionButtons[i].id;
+    });
+};
 
 selectDogButton.addEventListener("click", () => {
     selectPetScreen.style.display = "none";
     inGameScreen.style.display = "block";
 });
+
+/* IN GAME SCREEN */
 
 for (let i = 0; i < actionButtons.length; i++) {
     actionButtons[i].addEventListener("click", () => {
